4579fed24a101d0bacd6861ad2f50089d1513b68
jonahcoleman23 <jonahcoleman23@gmail.com>
2019-11-10 18:50:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist[aritstName]
+  delete playlist[artistName]
   return playlist
 }
